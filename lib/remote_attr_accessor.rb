85c9d9648bed0d58e64597042fd608e5c47c863e
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2016-06-02 12:28:03 +0900

    reorder require order

--- lib/remote_attr_accessor.rb
+++ lib/remote_attr_accessor.rb
@@ -1,4 +1,9 @@
-require 'remote_attr_accessor/attrs'
-
 module RemoteAttrAccessor
 end
+
+require_relative 'remote_attr_accessor/config'
+require_relative 'remote_attr_accessor/base'
+require_relative 'remote_attr_accessor/api'
+require_relative 'remote_attr_accessor/attrs'
+require_relative 'remote_attr_accessor/bulk'
+require_relative 'remote_attr_accessor/version'
