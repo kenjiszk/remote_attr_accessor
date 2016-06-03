ee549c977d2e50006e2c797653518f27f4b87f47
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2016-06-03 16:24:27 +0900

    add empty mock

---
+++ spec/remote_attr_accessor/attr_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'get remote attribute' do
+  it 'add instance variable' do
+  end
+end
+
+describe 'put remote attribute' do
+  it 'purge instance variable' do
+  end
+end
