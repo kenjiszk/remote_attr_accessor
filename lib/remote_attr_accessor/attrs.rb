d071ba1063a92642816f540f19dde1e471c0fc65
kenji.suzuki <kenji.suzuki.1984@gmail.com>
2016-06-03 09:34:11 +0900

    add method_missiong

--- lib/remote_attr_accessor/attrs.rb
+++ lib/remote_attr_accessor/attrs.rb
@@ -1,25 +1,34 @@
 module RemoteAttrAccessor::Attrs
   extend RemoteAttrAccessor::Base
 
-  config.remote_attrs.each do |attr|
-    attr_with_prefix = config.prefix + attr.to_s
-    define_method(attr_with_prefix) do
-      config = RemoteAttrAccessor::Config
-      remote_id = eval(config.id_name).to_s
-      eval("@#{attr_with_prefix}") ||
-        instance_variable_set(
-          "@#{attr_with_prefix}", 
-          RemoteAttrAccessor::Api.get_remote_attrs([remote_id])[config.remote_json_key][remote_id][attr.to_s]
-        )
+  # Tricky
+  def method_missing(name, *args)
+    # skip if name is not included in remote_attrs_with_prefix
+    super unless config.remote_attrs_with_prefix.grep(/#{name}/)
+
+    # attr methods are defined after configs are overwritten
+    config.remote_attrs.each do |attr|
+      attr_with_prefix = config.prefix + attr.to_s
+      define_method(attr_with_prefix) do
+        config = RemoteAttrAccessor::Config
+        remote_id = eval(config.id_name).to_s
+        eval("@#{attr_with_prefix}") ||
+          instance_variable_set(
+            "@#{attr_with_prefix}", 
+            RemoteAttrAccessor::Api.get_remote_attrs([remote_id])[config.remote_json_key][remote_id][attr.to_s]
+          )
+      end
+
+      define_method("#{attr_with_prefix}=") do |val|
+        config = RemoteAttrAccessor::Config
+        params = Hash.new
+        params[config.id_name] = eval(config.id_name).to_s
+        params[attr.to_s] = val
+        RemoteAttrAccessor::Api.upsert_remote_attr(params)
+        instance_variable_set("@#{attr_with_prefix}", nil)
+      end
     end
 
-    define_method("#{attr_with_prefix}=") do |val|
-      config = RemoteAttrAccessor::Config
-      params = Hash.new
-      params[config.id_name] = eval(config.id_name).to_s
-      params[attr.to_s] = val
-      RemoteAttrAccessor::Api.upsert_remote_attr(params)
-      instance_variable_set("@#{attr_with_prefix}", nil)
-    end
+    send(name, *args)
   end
 end
